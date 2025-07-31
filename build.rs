4b96de1c8391186804cd24738abc72e50ad172a1
Libor Jonat <libor.jonat@quantlane.com>
2025-07-31 15:22:13 +0200

    Tests are working

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!("cargo:rustc-link-arg-bins=--nmagic");
-    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+    println!("cargo:rustc-link-arg=--nmagic");
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
 }
